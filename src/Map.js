c647a98bc53eb9d3a3bd1336d50bfe568e11aa27
Sid Natarajan <sid.nat@gmail.com>
2023-01-26 23:48:34 -0500

    Map component, previous commit is AddMap component

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,33 @@
 import { Box } from "@mui/system";
-
-
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 const Map = props => {
-    //3 usestates
-    //useeffect with empty dependency array to make the call to get map data from express server and show data in return
-{/* load hardcoded uuid c94cbfd0-ccc9-40d7-a1c9-57b6390efb93 from backend and show the image, mapName, Creator on screen*/}
+	const [map, setMap] = useState(null)
+	const [pins, setPins] = useState(null)
+	let { id } = useParams();
 
-    return (
-        <Box>
-            <TextField>{mapName}</TextField>
-            <TextField>{mapName}</TextField>
-            <img src={imageLink} />
-        </Box>
-        )
+	useEffect(() => {
+		axios.get(`http://localhost:3003/getMapAndPins?uuid=${id}`)
+			.then((results) => {
+				console.log('results', results)
+				setMap(results.map)
+				setPins(results.pins)
+			})
+	}, [])
+
+
+	//3 usestates
+	{/* load hardcoded uuid c94cbfd0-ccc9-40d7-a1c9-57b6390efb93 from backend and show the image, mapName, Creator on screen*/ }
+
+	return (
+		<Box>
+			<h5>{JSON.stringify(map)}</h5>
+			{/* <TextField>{creator}</TextField>
+            <TextField>{mapName}</TextField> */}
+			{/* <img src={imageLink} /> */}
+		</Box>
+	)
 }
 
 export default Map;
