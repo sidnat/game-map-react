d533360e4dd0453bfbac2540d982366a09b32e3c
Sid Natarajan <sid.nat@gmail.com>
2023-02-28 14:52:49 -0500

    leaflet marker icons

---
+++ src/utils/markerIcons.js
@@ -0,0 +1,239 @@
+import L from 'leaflet';
+
+const iconWarning = new L.Icon({
+    iconUrl: require('../assets/warning.png'),
+    iconRetinaUrl: require('../assets/warning.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(35, 35),
+});
+
+const iconBrown = new L.Icon({
+    iconUrl: require('../assets/brown-613a00.png'),
+    iconRetinaUrl: require('../assets/brown-613a00.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconRedDark = new L.Icon({
+    iconUrl: require('../assets/red-dark-6b1614.png'),
+    iconRetinaUrl: require('../assets/red-dark-6b1614.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconRed = new L.Icon({
+    iconUrl: require('../assets/red-930000.png'),
+    iconRetinaUrl: require('../assets/red-930000.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconRedLight = new L.Icon({
+    iconUrl: require('../assets/red-light-c92a2a.png'),
+    iconRetinaUrl: require('../assets/red-light-c92a2a.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconOrange = new L.Icon({
+    iconUrl: require('../assets/orange-db3e00.png'),
+    iconRetinaUrl: require('../assets/orange-db3e00.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconOrangeLight = new L.Icon({
+    iconUrl: require('../assets/orange-light-ff9800.png'),
+    iconRetinaUrl: require('../assets/orange-light-ff9800.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconYellow = new L.Icon({
+    iconUrl: require('../assets/yellow-fccb00.png'),
+    iconRetinaUrl: require('../assets/yellow-fccb00.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconGreenDark = new L.Icon({
+    iconUrl: require('../assets/green-dark-23482f.png'),
+    iconRetinaUrl: require('../assets/green-dark-23482f.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconGreen = new L.Icon({
+    iconUrl: require('../assets/green-008b02.png'),
+    iconRetinaUrl: require('../assets/green-008b02.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconGreenLight = new L.Icon({
+    iconUrl: require('../assets/green-light-8bc34a.png'),
+    iconRetinaUrl: require('../assets/green-light-8bc34a.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconGreenNeon = new L.Icon({
+    iconUrl: require('../assets/green-neon-37d67a.png'),
+    iconRetinaUrl: require('../assets/green-neon-37d67a.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconBlueDark = new L.Icon({
+    iconUrl: require('../assets/blue-dark-1a237e.png'),
+    iconRetinaUrl: require('../assets/blue-dark-1a237e.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconBlueTeal = new L.Icon({
+    iconUrl: require('../assets/blue-teal-006b76.png'),
+    iconRetinaUrl: require('../assets/blue-teal-006b76.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconBlue = new L.Icon({
+    iconUrl: require('../assets/blue-1273de.png'),
+    iconRetinaUrl: require('../assets/blue-1273de.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconPink = new L.Icon({
+    iconUrl: require('../assets/pink-e91e63.png'),
+    iconRetinaUrl: require('../assets/pink-e91e63.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconPinkNeon = new L.Icon({
+    iconUrl: require('../assets/pink-neon-fa28ff.png'),
+    iconRetinaUrl: require('../assets/pink-neon-fa28ff.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconPurple = new L.Icon({
+    iconUrl: require('../assets/purple-5300eb.png'),
+    iconRetinaUrl: require('../assets/purple-5300eb.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconPurpleLight = new L.Icon({
+    iconUrl: require('../assets/purple-light-9c27b0.png'),
+    iconRetinaUrl: require('../assets/purple-light-9c27b0.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconBlack = new L.Icon({
+    iconUrl: require('../assets/black-2a2d2f.png'),
+    iconRetinaUrl: require('../assets/black-2a2d2f.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+const iconWhite = new L.Icon({
+    iconUrl: require('../assets/white-cec6ba.png'),
+    iconRetinaUrl: require('../assets/white-cec6ba.png'),
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(27, 40),
+});
+
+export {
+    iconWarning,
+    iconBrown,
+    iconRedDark,
+    iconRed,
+    iconRedLight,
+    iconOrange,
+    iconOrangeLight,
+    iconYellow,
+    iconGreenDark,
+    iconGreen,
+    iconGreenLight,
+    iconGreenNeon,
+    iconBlueDark,
+    iconBlueTeal,
+    iconBlue,
+    iconPink,
+    iconPinkNeon,
+    iconPurple,
+    iconPurpleLight,
+    iconBlack,
+    iconWhite,
+};
+
+
+/*
+warning triangle
+brown 613a00
+red-dark 6b1614
+red 930000
+red-light c92a2a
+orange db3e00
+orange-light ff9800
+yellow fccb00
+green-dark 23482f
+green 008b02
+green-light 8bc34a
+green-neon 37d67a
+blue-dark 1a237e
+blue-teal 006b76
+blue 1273de
+pink e91e63
+pink-neon fa28ff
+purple 5300eb
+purple-light 9c27b0
+black 2a2d2f
+white cec6ba
+*/
