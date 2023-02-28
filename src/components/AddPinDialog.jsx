6e8da618259e05912639ea5ed737e9f108833d72
Sid Natarajan <sid.nat@gmail.com>
2023-02-28 14:51:41 -0500

    Add Pin Dialog component

--- src/components/AddPinDialog.jsx
+++ src/components/AddPinDialog.jsx
@@ -5,71 +5,143 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogTitle,
-  Autocomplete,
+  Select,
+  MenuItem,
+  Typography,
+  Grid,
+  Card,
+  InputLabel,
+  FormControl
 } from "@mui/material";
 import ColourPicker from "./ColourPicker";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import './AddPinDialog.css'
 
 const AddPinDialog = (props) => {
-  // replace with list of categories from props list of pins
-  const categories = [
-    { label: "House", colour: {
-        "r": 129,
-        "g": 112,
-        "b": 99,
-        "a": 1
-    } },
-    { label: "Mission", colour: {
-        "r": 129,
-        "g": 4,
-        "b": 99,
-        "a": 1
-    } },
-  ];
-
+  const {
+    categories,
+    uuid,
+    saveCategory,
+    deleteCategory,
+    savePin,
+    dialogOpen,
+    setDialogOpen
+  } = props
+  const [showAddCategory, setShowAddCategory] = useState(false)
   const [inputs, setInputs] = useState({
+    title: "",
     category: "",
-    description: "",
+    note: "",
   });
+  const [colour, setColour] = useState('#000000')
+
+  const onColourChange = (colour) => {
+    setColour(colour.hex)
+  }
 
   const onAdd = () => {
-    props.setDialogOpen(false);
-    props.savePin(inputs.category, inputs.description);
+    setDialogOpen(false);
+    savePin(uuid, inputs.title, inputs.category, inputs.note);
   };
 
   return (
-    <Dialog open={props.dialogOpen} onClose={() => props.setDialogOpen(false)}>
-      <DialogTitle>Add A Pin</DialogTitle>
+    <Dialog open={dialogOpen} onClose={() => props.setDialogOpen(false)}>
+      <Typography gutterBottom variant="h4" align="center">
+        Add New Pin
+      </Typography>
       <DialogContent>
-        <Autocomplete
-          disablePortal
-          freeSolo
-          options={categories}
-          sx={{ width: 300 }}
-          // set defualt value to the first available value from categories you populate above
-          value={inputs.category}
-          onChange={(e, newValue) => {
-            setInputs((prevState) => ({ ...prevState, category: newValue }));
-          }}
-          renderInput={(params) => <TextField {...params} label="Categories" />}
-        />
-        <ColourPicker />
-        <TextField
-          label="Description"
-          multiline
-          rows={4}
-          value={inputs.description}
-          onChange={(e) => {
-            setInputs((prevState) => ({
-              ...prevState,
-              description: e.target.value,
-            }));
-          }}
-        />
+        <Grid>
+          <Card style={{ maxWidth: 300, padding: "20px 5px", margin: "0 auto" }}>
+            <TextField
+              label="Title"
+              sx={{ width: 275 }}
+              onChange={(e) => {
+                setInputs((prevState) => ({
+                  ...prevState,
+                  title: e.target.value
+                }))
+              }}
+            >
+            </TextField>
+            <Button onClick={() => {
+              setShowAddCategory((prevState) => (!prevState))
+            }}>
+              {showAddCategory ? "select existing category" : "add new category"}
+            </Button>
+            {!showAddCategory ? (
+              <>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Categories</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Categories"
+                    sx={{ width: 275 }}
+                    onChange={(e, newValue) => {
+                      setInputs((prevState) => ({ ...prevState, category: newValue.props.value }));
+                    }}
+                  >
+                    {categories.map((category) => (
+                      <MenuItem key={category.id} value={category}>
+                        <div className="catDelete">
+                          {category.label}
+                          <FontAwesomeIcon icon={faX} onClick={() => {
+                            deleteCategory(category.id)
+                          }}
+                          />
+                        </div>
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </>
+            ) : (
+              <>
+                <TextField
+                  label="New Category"
+                  onChange={(e) => {
+                    setInputs((prevState) => ({ ...prevState, category: e.target.value }));
+                  }}
+                />
+                <ColourPicker onColourChange={onColourChange} />
+                <Button onClick={() => {
+                  if (inputs.category && colour) {
+                    if (saveCategory(inputs.category, colour)) {
+                      setShowAddCategory(false)
+                    }
+                  } else {
+                    alert('Category not saved')
+                  }
+                }}>
+                  Save Category
+                </Button>
+              </>
+            )}
+            <TextField
+              label="Note"
+              sx={{ width: 275 }}
+              multiline rows={4}
+              onChange={(e) => {
+                setInputs((prevState) => ({
+                  ...prevState,
+                  note: e.target.value
+                }));
+              }}
+            />
+          </Card>
+        </Grid>
       </DialogContent>
       <DialogActions>
         <Button onClick={() => props.setDialogOpen(false)}>Cancel</Button>
-        <Button onClick={() => onAdd()}>Add</Button>
+        <Button onClick={() => {
+          if (inputs.title && inputs.category && inputs.note) {
+            onAdd()
+          } else {
+            alert('Pin details not saved')
+          }
+        }}
+        >Add</Button>
       </DialogActions>
     </Dialog>
   );
