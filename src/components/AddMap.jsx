e40c9be0a72d10f528fff3177f2ce8563170bd0f
Sid Natarajan <sid.nat@gmail.com>
2023-03-10 18:24:03 -0500

    changed AddMap component

--- src/components/AddMap.jsx
+++ src/components/AddMap.jsx
@@ -1,11 +1,20 @@
+import * as React from 'react';
 import { useState } from 'react';
-import axios from '../utils/axios';
-import { AppBar, Paper, Box, Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material';
-import './AddMap.css'
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import instance from '../utils/axios';
 import landingImage from '../assets/landingImage.png'
 import { v4 as uuidv4 } from 'uuid'
 
-const AddMap = (props) => {
+const theme = createTheme();
+
+const AddMap = () => {
   const [mapName, setMapName] = useState("");
   const [creator, setCreator] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
@@ -13,9 +22,9 @@ const AddMap = (props) => {
   const onFileUpload = () => {
     const formData = new FormData();
     formData.append("media", selectedFile);
-    formData.append("key", "0000334f0bdb447fc1efc3a8efa304f7")
+    formData.append("apikey", "0000334f0bdb447fc1efc3a8efa304f7")
 
-    return axios.post("https://thumbsnap.com/api/upload", formData)
+    return instance.post("https://thumbsnap.com/api/upload", formData)
   }
 
   const handleSave = () => {
@@ -25,139 +34,120 @@ const AddMap = (props) => {
       creator,
     }
 
-    console.log(map)
-
     onFileUpload()
       .then((res) => {
-
-        console.log('resdata', res.data)
-        console.log('resurl', res.data.url)
-
-        axios.post("addMap", {
+        console.log('test')
+        instance.post("addMap", {
           ...map,
           imageLink: res.data.image.url
         })
         // bookmark page prompt
       })
-      .catch((err) => alert("Failed") && console.log(err));
+      .catch((err) => console.log(err));
   }
 
   return (
-    <div>
-      <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
-      <button onClick={(e) => handleSave()}>
-        Upload!
-      </button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${landingImage})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h3">
+              GameMap
+            </Typography>
+            <br />
+            <Typography component="h1" variant="h6">
+              Add your own High-res map image, and place pins wherever you want!
+            </Typography>
+            <Typography component="h1" variant="h6">
+              Create your map by submitting the following:
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                fullWidth
+                id="outlined-basic"
+                label="Map Title"
+                placeholder="example: GTA V, RDR2, Hogwarts Legacy..."
+                autoFocus
+                onChange={(e) => setMapName(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                fullWidth
+                id="outlined-basic"
+                label="Creator"
+                placeholder="example: FirstName LastName"
+                onChange={(e) => setCreator(e.target.value)}
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                component="label"
+              >
+                Upload Map Image
+                <input
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => setSelectedFile(e.target.files[0])}
+                  hidden
+                />
+              </Button>
+              <TextField
+                id="outlined-read-only-input"
+                value={selectedFile?.name}
+                placeholder="ExampleFileName.jpeg"
+                variant="outlined"
+                fullWidth required
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => {
+                  if (mapName && creator && selectedFile) {
+                    handleSave()
+                  }
+                  // } else {
+                  //   alert('Map details not saved')
+                  // }
+                }}
+              >
+                Create Map
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
-
-  // return (
-  //   <>
-  //     <AppBar position="static">
-  //       <Typography className="navbar" variant="h3" component="div" sx={{ flexGrow: 1 }}>
-  //         GameMap
-  //       </Typography>
-  //     </AppBar>
-  //     <div className="landing">
-  //       <div>
-  //         <Box
-  //           component="form"
-  //           noValidate
-  //           autoComplete="off"
-  //           className="appBox"
-  //         >
-  //           <Paper elevation={3} >
-  //             <Grid>
-  //               <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
-  //                 <Typography gutterBottom variant="h5" align="center">
-  //                   Add your own High-res map image, and place pins wherever you want!
-  //                 </Typography>
-  //                 <CardContent>
-  //                   <Typography gutterBottom variant="h6" align="center">
-  //                     Create your map by submitting the following:
-  //                   </Typography>
-  //                   {/* <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
-  //                   Fill up the form and our team will get back to you within 24 hours.
-  //                 </Typography> */}
-  //                   <form>
-  //                     <Grid container spacing={2}>
-  //                       <Grid xs={12} item>
-  //                         <TextField
-  //                           id="outlined-basic"
-  //                           placeholder="example: GTA V, RDR2, Hogwarts Legacy..."
-  //                           label="Map Title"
-  //                           variant="outlined"
-  //                           fullWidth required
-  //                           onChange={(e) => setMapName(e.target.value)}
-  //                         />
-  //                       </Grid>
-  //                       <Grid xs={12} item>
-  //                         <TextField
-  //                           id="outlined-basic"
-  //                           label="Creator"
-  //                           placeholder="example: FirstName LastName"
-  //                           variant="outlined"
-  //                           fullWidth required
-  //                           onChange={(e) => setCreator(e.target.value)}
-  //                         />
-  //                       </Grid>
-  //                       <Grid item xs={12}>
-  //                         <Button
-  //                           variant="contained"
-  //                           component="label"
-  //                         >
-  //                           Upload Map Image
-  //                           <input
-  //                             accept="image/*"
-  //                             type="file"
-  //                             onChange={(e) => setSelectedFile(e.target.files[0])}
-  //                             hidden
-  //                           />
-  //                         </Button>
-  //                       </Grid>
-  //                       <Grid xs={12} item>
-  //                         <TextField
-  //                           id="outlined-read-only-input"
-  //                           value={selectedFile?.name}
-  //                           placeholder="ExampleFileName.jpeg"
-  //                           variant="outlined"
-  //                           fullWidth required
-  //                           InputProps={{
-  //                             readOnly: true,
-  //                           }}
-  //                         />
-  //                       </Grid>
-  //                       <Grid item xs={12}>
-  //                         <Button
-  //                           type="submit"
-  //                           variant="contained"
-  //                           color="primary"
-  //                           fullWidth
-  //                           onClick={() => {
-  //                             console.log(selectedFile)
-  //                             if (mapName && creator && selectedFile) {
-  //                               handleSave()
-  //                             } else {
-  //                               alert('Map details not saved')
-  //                             }
-  //                           }}
-  //                         >Create Map</Button>
-  //                       </Grid>
-
-  //                     </Grid>
-  //                   </form>
-  //                 </CardContent>
-  //               </Card>
-  //             </Grid>
-  //           </Paper>
-  //         </Box>
-  //       </div>
-  //       <div>
-  //         <img src={landingImage} width="497em" alt="GTA V Pin map example" />
-  //       </div>
-  //     </div>
-  //   </>
-  // );
 }
 
-export default AddMap;
+export default AddMap
---
+++ src/components/AddMap3.jsx
@@ -0,0 +1,165 @@
+import { useState } from 'react';
+import instance from '../utils/axios';
+import { AppBar, Paper, Box, Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material';
+import './AddMap.css'
+import landingImage from '../assets/landingImage.png'
+import { v4 as uuidv4 } from 'uuid'
+
+const AddMap = (props) => {
+  const [mapName, setMapName] = useState("");
+  const [creator, setCreator] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const onFileUpload = () => {
+    const formData = new FormData();
+    formData.append("media", selectedFile);
+    formData.append("key", "0000334f0bdb447fc1efc3a8efa304f7")
+
+    return instance.post("https://thumbsnap.com/api/upload", formData)
+  }
+
+  const handleSave = () => {
+    const map = {
+      id: uuidv4(),
+      mapName,
+      creator,
+    }
+
+    console.log(map)
+
+    onFileUpload()
+      .then((res) => {
+
+        console.log('resdata', res.data)
+        console.log('resurl', res.data.url)
+
+        instance.post("addMap", {
+          ...map,
+          imageLink: res.data.image.url
+        })
+        // bookmark page prompt
+      })
+      .catch((err) => alert("Failed") && console.log(err));
+  }
+
+  // return (
+  //   <div>
+  //     <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
+  //     <button onClick={(e) => handleSave()}>
+  //       Upload!
+  //     </button>
+  //   </div>
+  // );
+
+  return (
+    <>
+      <AppBar position="static">
+        <Typography className="navbar" variant="h3" component="div" sx={{ flexGrow: 1 }}>
+          GameMap
+        </Typography>
+      </AppBar>
+      <div className="landing">
+        <div>
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            className="appBox"
+          >
+            <Paper elevation={3} >
+              <Grid>
+                <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+                  <Typography gutterBottom variant="h5" align="center">
+                    Add your own High-res map image, and place pins wherever you want!
+                  </Typography>
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" align="center">
+                      Create your map by submitting the following:
+                    </Typography>
+                    {/* <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
+                    Fill up the form and our team will get back to you within 24 hours.
+                  </Typography> */}
+                    <form>
+                      <Grid container spacing={2}>
+                        <Grid xs={12} item>
+                          <TextField
+                            id="outlined-basic"
+                            placeholder="example: GTA V, RDR2, Hogwarts Legacy..."
+                            label="Map Title"
+                            variant="outlined"
+                            fullWidth required
+                            onChange={(e) => setMapName(e.target.value)}
+                          />
+                        </Grid>
+                        <Grid xs={12} item>
+                          <TextField
+                            id="outlined-basic"
+                            label="Creator"
+                            placeholder="example: FirstName LastName"
+                            variant="outlined"
+                            fullWidth required
+                            onChange={(e) => setCreator(e.target.value)}
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Button
+                            variant="contained"
+                            component="label"
+                          >
+                            Upload Map Image
+                            <input
+                              accept="image/*"
+                              type="file"
+                              onChange={(e) => setSelectedFile(e.target.files[0])}
+                              hidden
+                            />
+                          </Button>
+                        </Grid>
+                        <Grid xs={12} item>
+                          <TextField
+                            id="outlined-read-only-input"
+                            value={selectedFile?.name}
+                            placeholder="ExampleFileName.jpeg"
+                            variant="outlined"
+                            fullWidth required
+                            InputProps={{
+                              readOnly: true,
+                            }}
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            onClick={() => {
+                              console.log(selectedFile)
+                              if (mapName && creator && selectedFile) {
+                                handleSave()
+                              } else {
+                                alert('Map details not saved')
+                              }
+                            }}
+                          >
+                            Create Map
+                          </Button>
+                        </Grid>
+
+                      </Grid>
+                    </form>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Paper>
+          </Box>
+        </div>
+        <div>
+          <img src={landingImage} width="497em" alt="GTA V Pin map example" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AddMap;
